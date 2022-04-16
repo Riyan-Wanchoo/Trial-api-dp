f75c52680270ce1a2050bf557793e537022dd65d
Riyan <riyanwanchoo1072008@yahoo.com>
2022-04-16 10:24:23 +0530

    f

--- routes/fetchNews.js
+++ routes/fetchNews.js
@@ -9,32 +9,33 @@ const chooseNews = 19
 
 router.get('/', async (req, res)=>{
     console.log("im coming here")
-    const filter = {}
-    const allNews = await News.find(filter).sort({_id: -1})
+    // const filter = {}
+    // const allNews = await News.find(filter).sort({_id: -1})
 
-    let topNews = []
-    let latestNews = []
-    let mal = []
-    for (let index = 0; index < chooseNews; index++) {
-        if (index>=0 && index<=4) {
-            latestNews.push(allNews[index])
-        }
-        else if (index>4 && index<=14) {
-            topNews.push(allNews[index])
-        }
-        else if (index>14) {
-            mal.push(allNews[index])
-        }
-    }
+    // let topNews = []
+    // let latestNews = []
+    // let mal = []
+    // for (let index = 0; index < chooseNews; index++) {
+    //     if (index>=0 && index<=4) {
+    //         latestNews.push(allNews[index])
+    //     }
+    //     else if (index>4 && index<=14) {
+    //         topNews.push(allNews[index])
+    //     }
+    //     else if (index>14) {
+    //         mal.push(allNews[index])
+    //     }
+    // }
 
-    const stories = await Stories.find(filter)
+    // const stories = await Stories.find(filter)
 
-    const allData = {
-        topNews: topNews,
-        latestNews: latestNews,
-        mal: mal,
-        stories: stories
-    }
+    // const allData = {
+    //     topNews: topNews,
+    //     latestNews: latestNews,
+    //     mal: mal,
+    //     stories: stories
+    // }
+    const allData = await News.find({}).limit(1)
     res.status(200).json(allData)
 })
 
